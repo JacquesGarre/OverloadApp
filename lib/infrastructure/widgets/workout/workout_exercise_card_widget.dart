77d776b52f0295bff14a38dfa569c943a9f8d60b
Jaks <jcqs.gr@gmail.com>
2025-01-19 22:13:19 +0100

    workout exercise

--- lib/infrastructure/widgets/workout/workout_exercise_card_widget.dart
+++ lib/infrastructure/widgets/workout/workout_exercise_card_widget.dart
@@ -12,14 +12,13 @@ class WorkoutExerciseCardWidget extends StatelessWidget {
   final bool readonly;
   final void Function(WorkoutExercise workoutExercise) onWorkoutExerciseRemoved;
 
-  const WorkoutExerciseCardWidget({
-    super.key,
-    required this.workoutExercise,
-    required this.checkable,
-    required this.setsNumberSelector,
-    required this.readonly,
-    required this.onWorkoutExerciseRemoved
-  });
+  const WorkoutExerciseCardWidget(
+      {super.key,
+      required this.workoutExercise,
+      required this.checkable,
+      required this.setsNumberSelector,
+      required this.readonly,
+      required this.onWorkoutExerciseRemoved});
 
   _removeWorkoutExercise() {
     onWorkoutExerciseRemoved(workoutExercise);
@@ -44,7 +43,7 @@ class WorkoutExerciseCardWidget extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.fromLTRB(6.0, 0.0, 0.0, 0.0),
                         child: Text(
-                          "${workoutExercise.exercise().name().value()} (index:${workoutExercise.index()})}",
+                          workoutExercise.exercise().name().value(),
                           style: const TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w600,
@@ -52,6 +51,23 @@ class WorkoutExerciseCardWidget extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 5.0),
+                      if (workoutExercise.goals() != null)
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(
+                            6.0,
+                            0.0,
+                            0.0,
+                            0.0,
+                          ),
+                          child: Text(
+                            '${workoutExercise.goals()!.count()} objective${workoutExercise.goals()!.count() > 1 ? 's' : ''} left',
+                            style: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w400,
+                              color: AppColorScheme.primary,
+                            ),
+                          ),
+                        ),
                     ],
                   ),
                 ),
@@ -75,7 +91,8 @@ class WorkoutExerciseCardWidget extends StatelessWidget {
             SetsTableWidget(
               key: ValueKey(workoutExercise.id().value()),
               exercise: workoutExercise.exercise(),
-              sets: workoutExercise.goals() != null && workoutExercise.goals()!.count() > 0
+              sets: workoutExercise.goals() != null &&
+                      workoutExercise.goals()!.count() > 0
                   ? workoutExercise.goals()!.value().first.sets()
                   : workoutExercise.sets(),
               checkable: checkable,
