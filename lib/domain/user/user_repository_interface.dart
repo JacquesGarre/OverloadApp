7da203912c7eb7c98fe91522bf0e2853d764cae5
Jaks <jcqs.gr@gmail.com>
2025-02-14 20:55:49 +0100

    user repo

---
+++ lib/domain/user/user_repository_interface.dart
@@ -0,0 +1,9 @@
+import 'package:overload/domain/user/id.dart';
+import 'package:overload/domain/user/user.dart';
+
+abstract class UserRepositoryInterface {
+  Future<void> add(User user);
+  Future<void> update(User user);
+  Future<void> delete(User user);
+  Future<User?> ofId(Id id);
+}
